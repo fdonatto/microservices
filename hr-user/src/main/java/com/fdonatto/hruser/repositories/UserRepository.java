3f58047988edf11f7dabc4235f7615d6cc47b840
fdonatto <fdonatto@gmail.com>
2021-05-16 15:00:34 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/fdonatto/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fdonatto.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fdonatto.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+}
